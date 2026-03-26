f8586685e54aa3897737ea60653f1bc5f2cfd50d
armcool007 <bipinkk004@gmail.com>
2026-03-26 23:34:50 +0530

    Update eksctl.sh

--- packages/eksctl.sh
+++ packages/eksctl.sh
@@ -2,3 +2,7 @@
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
+
+#TO CREATE A CLUSTER WITH ONLY ONE MACHINE WITH SIZE 20GB:
+  eksctl create cluster --name karma --region ap-south-1 --node-type t3.small --nodes 1 --nodes-min 1 --nodes-max 1 --node-volume-size 20
+
